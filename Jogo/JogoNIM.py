72ade90d1c30e692d4fe02ae9d788e53080252f8
Vinicios Toledo <viabolafio@hotmail.com>
2021-06-29 17:40:02 -0300

    ajustes para o teste do curso

--- Jogo/JogoNIM.py
+++ Jogo/JogoNIM.py
@@ -12,10 +12,10 @@ def usuario_escolhe_jogada(n,m):  #Definição para o jogador escolher sua jogad
     jogada_valida = 0
     
     while jogada_valida == 0:  #Repetição para testar se a jogada é valida
-        jogada_valida = int(input('Digite sua jogada: \n'))
+        jogada_valida = int(input('Quantas peças você vai tirar? \n'))
         
-        if jogada_valida > m or jogada_valida > n or jogada_valida == 0:
-            print('Jogada invalida, digite novamente: ')
+        if jogada_valida > m or jogada_valida > n or jogada_valida <= 0:
+            print('Oops! Jogada inválida! Tente de novo.')
             jogada_valida = 0
         
         else:  #Retorna da jogada caso ela seja valida.
@@ -23,13 +23,8 @@ def usuario_escolhe_jogada(n,m):  #Definição para o jogador escolher sua jogad
 
 def partida():  #Definição que cria uma partida
 
-    n = int(input('Digite o número de peças iniciais: ')) #variavel obrigatoria
-    m = int(input('Digite quantas peças podem ser retiradas por rodada: ')) #variavel obrigatoria
-
-    while m > n or m == n: #Repetição para verificar se os valores n e m são validos.
-        print('O número de peças inicias deve ser maior que o número de peças que podem ser retiradas\n')
-        n = int(input('Digite o número de peças iniciais: ')) #variavel obrigatoria
-        m = int(input('Digite quantas peças podem ser retiradas por rodada: ')) #variavel obrigatoria
+    n = int(input('Quantas peças? ')) #variavel obrigatoria
+    m = int(input('Limite de peças por jogada? ')) #variavel obrigatoria
 
     jogadaDoCoumputador = 0  #Variavel para gravar as jogadas durante a partida
     jogadaDoUsuario = 0  #Variavel para gravar as jogadas durante a partida
@@ -40,30 +35,33 @@ def partida():  #Definição que cria uma partida
         while n > 0:  #Repetição para a partida acabar quando as peças acabarem
             jogadaDoUsuario = usuario_escolhe_jogada(n,m)
             n = n - jogadaDoUsuario
-            print(f'O jogador removeu {jogadaDoUsuario} peças, restam {n} peças\n')
+            print(f'Você removeu {jogadaDoUsuario} peças')
+            print(f'Agora restam {n} peças no tabuleiro\n')
 
             jogadaDoCoumputador = computador_escolhe_jogada(n,m)
             n = n - jogadaDoCoumputador
-            print(f'O computador removeu {jogadaDoCoumputador} peças, restam {n} peças\n')
+            print(f'O computador tirou {jogadaDoCoumputador} peças')
+            print(f'Agora restam {n} peças no tabuleiro\n')
 
         if n == 0:
-            print('O comptador venceu.\n')
+            print('Fim do jogo! O computador ganhou!\n')
 
     else:
 
-        print('\nComputador começa!!\n')
+        print('\nComputador começa!\n')
 
         while n > 0:  #Repetição para a partida acabar quando as peças acabarem
             jogadaDoCoumputador = computador_escolhe_jogada(n,m)
             n = n - jogadaDoCoumputador
-            print(f'O computador removeu {jogadaDoCoumputador} peças, restam {n} peças\n')
-            
+            print(f'O computador removeu {jogadaDoCoumputador} peças.')
+            print(f'Agora restam {n} peças no tabuleiro\n')
             if n > 0:
                 jogadaDoUsuario = usuario_escolhe_jogada(n,m)
                 n = n - jogadaDoUsuario
-                print(f'O jogador removeu {jogadaDoUsuario} peças, restam {n} peças\n')
+                print(f'Você removeu {jogadaDoUsuario} peças')
+                print(f'Agora restam {n} peças no tabuleiro\n')
             else:
-                print('O comptador venceu.\n')
+                print('Fim do jogo! O computador ganhou!\n')
 
 
 def campeonato():  #Definição que chama 3 partidas em sequencia. E acumula vitorias no placar
@@ -71,7 +69,7 @@ def campeonato():  #Definição que chama 3 partidas em sequencia. E acumula vit
     rodada = 1  #Variavel para salvar qual rodada do campeonato está sendo jogada
 
     while vitoriasDoComputador < 3:  #Repetiçao para acabar o campeonato depois de 3 partidas
-        print(f'\nrodada {rodada}\n')
+        print(f'\n**** Rodada {rodada} ****\n')
         partida()
         vitoriasDoComputador += 1
         rodada += 1
@@ -79,11 +77,11 @@ def campeonato():  #Definição que chama 3 partidas em sequencia. E acumula vit
 
 
     
-print('######## BEM VINDO AO JOGO NIM ########\nModos de Jogo:\n')
+print('Bem-vindo ao jogo do NIM! Escolha:\n')
 escolha = 0  #Variavel para dar ao jogador a opção de modo de jogo
 
 while escolha < 1 or escolha > 2:   #Repetiçáo para checar se o modo escolhido é valido
-    escolha = int(input('1 - Para partida unica\n2 - Para campeonato\n'))
+    escolha = int(input('1 - para jogar uma partida isolada\n2 - para jogar um campeonato\n'))
 
     if escolha == 1:
         print(partida())
