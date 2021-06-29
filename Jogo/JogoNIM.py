06a2a3a66c15aafc1b89db2dab2da4b70a568314
Vinicios Toledo <viabolafio@hotmail.com>
2021-06-29 14:54:38 -0300

    Comentando o codigo

--- Jogo/JogoNIM.py
+++ Jogo/JogoNIM.py
@@ -1,31 +1,27 @@
-def computador_escolhe_jogada(n,m):
-    #Variavel para salvar a jogado do computador na memoria.
-    jogada = 0
+def computador_escolhe_jogada(n,m):  #Definição para determinar a jogada do PC
+    jogada = 0   #Variavel para salvar a jogado do computador na memoria.
     
-    #Repetição para testar a estrategia vencedora.
-    while jogada != m and jogada != n:
+    while jogada != m and jogada != n:  #Repetição para testar a estrategia vencedora.
         jogada +=1
         
         if (n-jogada)%(m+1)== 0:
             return jogada
 
-def usuario_escolhe_jogada(n,m):
+def usuario_escolhe_jogada(n,m):  #Definição para o jogador escolher sua jogada
 
     jogada_valida = 0
     
-    #Repetição para testar se a jogada é valida
-    while jogada_valida == 0:
+    while jogada_valida == 0:  #Repetição para testar se a jogada é valida
         jogada_valida = int(input('Digite sua jogada: \n'))
         
         if jogada_valida > m or jogada_valida > n or jogada_valida == 0:
             print('Jogada invalida, digite novamente: ')
             jogada_valida = 0
         
-        #Retorna da jogada caso ela seja valida.
-        else:
+        else:  #Retorna da jogada caso ela seja valida.
             return jogada_valida
 
-def partida():
+def partida():  #Definição que cria uma partida
 
     n = int(input('Digite o número de peças iniciais: ')) #variavel obrigatoria
     m = int(input('Digite quantas peças podem ser retiradas por rodada: ')) #variavel obrigatoria
@@ -35,13 +31,13 @@ def partida():
         n = int(input('Digite o número de peças iniciais: ')) #variavel obrigatoria
         m = int(input('Digite quantas peças podem ser retiradas por rodada: ')) #variavel obrigatoria
 
-    jogadaDoCoumputador = 0
-    jogadaDoUsuario = 0
+    jogadaDoCoumputador = 0  #Variavel para gravar as jogadas durante a partida
+    jogadaDoUsuario = 0  #Variavel para gravar as jogadas durante a partida
         
-    if n % (m+1) == 0: #Teste para decidir quem começa segundo a estrategia vencedora
+    if n % (m+1) == 0:  #Teste para decidir quem começa segundo a estrategia vencedora
 
         print('Você começa!!\n')
-        while n > 0: #Repetição para a partida acabar quando as peças acabarem
+        while n > 0:  #Repetição para a partida acabar quando as peças acabarem
             jogadaDoUsuario = usuario_escolhe_jogada(n,m)
             n = n - jogadaDoUsuario
             print(f'O jogador removeu {jogadaDoUsuario} peças, restam {n} peças\n')
@@ -57,7 +53,7 @@ def partida():
 
         print('\nComputador começa!!\n')
 
-        while n > 0: #Repetição para a partida acabar quando as peças acabarem
+        while n > 0:  #Repetição para a partida acabar quando as peças acabarem
             jogadaDoCoumputador = computador_escolhe_jogada(n,m)
             n = n - jogadaDoCoumputador
             print(f'O computador removeu {jogadaDoCoumputador} peças, restam {n} peças\n')
@@ -70,11 +66,11 @@ def partida():
                 print('O comptador venceu.\n')
 
 
-def campeonato():
-    vitoriasDoComputador = 0
-    rodada = 1
+def campeonato():  #Definição que chama 3 partidas em sequencia. E acumula vitorias no placar
+    vitoriasDoComputador = 0  #Variavel para salvar os resutados 
+    rodada = 1  #Variavel para salvar qual rodada do campeonato está sendo jogada
 
-    while vitoriasDoComputador < 3: #Repetiçao para acabar o campeonato depois de 3 partidas
+    while vitoriasDoComputador < 3:  #Repetiçao para acabar o campeonato depois de 3 partidas
         print(f'\nrodada {rodada}\n')
         partida()
         vitoriasDoComputador += 1
@@ -84,9 +80,9 @@ def campeonato():
 
     
 print('######## BEM VINDO AO JOGO NIM ########\nModos de Jogo:\n')
-escolha = 0
+escolha = 0  #Variavel para dar ao jogador a opção de modo de jogo
 
-while escolha < 1 or escolha > 2: #Repetiçáo para checar se o modo escolhido é valido
+while escolha < 1 or escolha > 2:   #Repetiçáo para checar se o modo escolhido é valido
     escolha = int(input('1 - Para partida unica\n2 - Para campeonato\n'))
 
     if escolha == 1:
